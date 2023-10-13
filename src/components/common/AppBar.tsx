b88cf9dcf225b2effe840167cead6611e994433f
suprabathk <suprabathkondapally@gmail.com>
2023-10-13 14:34:40 +0530

    Added functionality to Sign Out button

--- src/components/common/AppBar.tsx
+++ src/components/common/AppBar.tsx
@@ -1,7 +1,15 @@
 import { AppBar, Box, Button, Container, Toolbar } from "@mui/material";
 import Logo from "./Logo";
+import { useRouter } from "next/router";
 
 const MyAppBar = () => {
+  const router = useRouter();
+
+  const signout = () => {
+    localStorage.removeItem("authToken");
+    router.push("/auth");
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="default" elevation={0}>
@@ -9,7 +17,7 @@ const MyAppBar = () => {
           <Container>
             <Logo />
           </Container>
-          <Button color="error" sx={{ whiteSpace: "nowrap" }}>
+          <Button color="error" sx={{ whiteSpace: "nowrap" }} onClick={signout}>
             Sign out
           </Button>
         </Toolbar>
