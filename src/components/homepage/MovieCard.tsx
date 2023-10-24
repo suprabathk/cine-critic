ad6afb4a363336bd9f8be652fb892947b87f850e
suprabathk <suprabathkondapally@gmail.com>
2023-10-25 01:31:57 +0530

    Fixed issue in MovieCard

--- src/components/homepage/MovieCard.tsx
+++ src/components/homepage/MovieCard.tsx
@@ -20,16 +20,7 @@ export default function ActionAreaCard(props: { movie: MoviePreview }) {
     release_date,
     vote_average,
     vote_count,
-    // } = props.movie;
-  } = {
-    adult: false,
-    overview: "df",
-    poster_path: "",
-    release_date: "",
-    title: "movieTitle",
-    vote_average: 0,
-    vote_count: 0,
-  };
+  } = props.movie;
 
   return (
     <Card className={movie.movieCard}>
