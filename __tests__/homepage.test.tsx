04d8ef8dfc6154293fe2350f9a1eb76e67342c29
suprabathk <suprabathkondapally@gmail.com>
2023-10-26 22:51:22 +0530

    Snapshot testing for Home Page

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -1,7 +1,7 @@
 import MovieBanner from "@/components/homepage/MovieBanner";
 import ActionAreaCard from "@/components/homepage/MovieCard";
-import Home from "@/pages";
-import { render, screen, waitFor } from "@testing-library/react";
+import Discover from "@/sections/homepage/Discover";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import renderer from "react-test-renderer";
 
@@ -38,32 +38,6 @@ jest.mock("next/router", () => ({
   },
 }));
 
-jest.mock("../src/hooks/useNowPlayingQuery", () => ({
-  useNowPlayingQuery: jest.fn().mockReturnValue({
-    data: {
-      results: [
-        {
-          adult: false,
-          backdrop_path: "",
-          genre_ids: [],
-          id: 1,
-          original_language: "original_language",
-          original_title: "original_title",
-          overview: "overview",
-          popularity: 0,
-          poster_path: "",
-          release_date: "release_date",
-          title: "title",
-          vote_average: 0,
-          vote_count: 0,
-        },
-      ],
-    },
-    isLoading: false,
-    error: {},
-  }),
-}));
-
 jest.mock("../src/hooks/useDiscoverQuery", () => ({
   useDiscoverQuery: jest.fn().mockReturnValue({
     data: {
@@ -91,6 +65,14 @@ jest.mock("../src/hooks/useDiscoverQuery", () => ({
 }));
 
 describe("Homepage tests", () => {
+  it("Snapshot testing", async () => {
+    let tree;
+    await act(async () => {
+      tree = renderer.create(<Discover />);
+    });
+    expect(tree!.toJSON()).toMatchSnapshot();
+  });
+
   describe("Movie banner tests", () => {
     it("Redirect on click", async () => {
       render(<MovieBanner movie={dummyMovieData} />);
